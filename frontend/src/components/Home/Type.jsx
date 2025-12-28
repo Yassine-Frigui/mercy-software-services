8e89d8134657c06462064febc0106cce1fde33a5
Youssef_Frigui <youssef.frigui13@gmail.com>
2025-12-28 13:15:26 +0100

    fuck this site and everyone that worked on it

--- frontend/src/components/Home/Type.jsx
+++ frontend/src/components/Home/Type.jsx
@@ -1,15 +1,13 @@
 import React from "react";
 import Typewriter from "typewriter-effect";
+import { useTranslation } from "react-i18next";
 
 function Type() {
+  const { t } = useTranslation();
   return (
     <Typewriter
       options={{
-        strings: [
-          "Full Stack Developer",
-          "UI/UX Designer",
-          "Software Engineer"
-        ],
+        strings: t('type.strings', { returnObjects: true }),
         autoStart: true,
         loop: true,
         deleteSpeed: 50,
